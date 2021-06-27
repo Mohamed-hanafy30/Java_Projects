4c66f3197b11ade6bdd9b9d7fed96cbdccad77c9
Kimo Store <Kimo Store@MoustafaPc>
2021-06-27 10:00:57 +0200

    second commit

--- Pyramids/src/pyramids/Pyramids.java
+++ Pyramids/src/pyramids/Pyramids.java
@@ -24,35 +24,19 @@ public class Pyramids {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
         PyramidsCSVDAOImpl obj = new PyramidsCSVDAOImpl();
 		
-		// please define here the path of the CSV file
 		String path = "C:\\Users\\Kimo Store\\Downloads\\pyramids.csv";
-		
-		// create a list of pyramids
 		List<Pyramid> l = obj.readPyramidsFromCSV(path);
-		
-		// print the list of pyramids
 		for(Pyramid p: l) {
 			System.out.println(p);
-		}//*/
-		
-		// sort according to pyramid's height
+		}
 		List<Pyramid> sortedPyramids = obj.sortPyramids(l);
-		
-		// print the sorted list of pyramids
 		for(Pyramid p: sortedPyramids) {
 			System.out.println(p);
-		}//*/
-		
-		
-		// get each location and the corresponding pyramids count
-		Map<String, Integer> locCount = obj.getSiteCount(l);
-		
-		// print the result
-		locCount.forEach((k, v) -> System.out.println("key="+ k+ ", value="+ v));
+		}
+		Map<String, Integer> countPerLocation = obj.getSiteCount(l);
+		countPerLocation.forEach((k, v) -> System.out.println("LOCATION: "+ k+ ", Count "+ v));
 
 	}
     }
